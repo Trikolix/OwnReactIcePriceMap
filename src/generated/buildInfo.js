ce3ee973168f02c45e23ecb2f0318f494c7e441f
Christian Helbig <christian.helbig@intenta-automotive.de>
2026-03-25 08:35:09 +0100

    Fix SPA rewrite handling for static assets

--- src/generated/buildInfo.js
+++ src/generated/buildInfo.js
@@ -1,3 +1,3 @@
 export const APP_VERSION = "2.1.3";
-export const APP_BUILD_ID = "2.1.3-1774423105";
-export const APP_BUILD_TIME = "2026-03-25T07:18:25.611Z";
+export const APP_BUILD_ID = "2.1.3-1774423921";
+export const APP_BUILD_TIME = "2026-03-25T07:32:01.625Z";
